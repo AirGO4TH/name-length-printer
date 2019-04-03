d66d70a9a3ec196d2127df853c3f7dfcfdf6f3b6
AirGO4TH <49202561+AirGO4TH@users.noreply.github.com>
2019-04-03 08:30:43 -0700

    Add files via upload

---
+++ yourName.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class yourName{
+	public static void main(String[] args){
+	    Scanner input = new Scanner(System.in);
+        System.out.println("What is your name?");
+        String name = input.next();
+        System.out.println("Hello. Your name has "+name.length()+" characters.");
+	}
+}
+
+
